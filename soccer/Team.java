4b5729c0757986f2fcfd8b159212c7a2787b837a
Filip <filipvelkovski12345@gmail.com>
2021-12-05 19:53:24 +0100

    Commiting second practice

--- soccer/Team.java
+++ soccer/Team.java
@@ -1,8 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
 package soccer;
 
+/**
+ *
+ * @author Administrator
+ */
 public class Team {
-	
-	public String teamName;
-	public Player[] playerArray; 
-
+    
+    public String teamName;
+    public Player[] playerArray;
+    
 }
